75872bb9d6c288661e4e6b9e69c3a98bc6556411
L'inspecteur <48238287+Linspecteur@users.noreply.github.com>
2021-05-27 14:34:46 +0200

    Add files via upload

---
+++ blood_sportracer/client.lua
@@ -0,0 +1,12 @@
+Citizen.CreateThread(function()
+
+RequestIpl("imp_impexp_interior_placement_interior_1_impexp")
+interiorID = GetInteriorAtCoords(941.10520000,-971.67590000,39.39997000)
+	if IsValidInterior(interiorID) then
+	
+		EnableInteriorProp(interiorID, "branded_style_set")
+		EnableInteriorProp(interiorID, "car_floor_hatch")
+		EnableInteriorProp(interiorID, "door_blocker")
+		RefreshInterior(interiorID)
+	end
+end)
